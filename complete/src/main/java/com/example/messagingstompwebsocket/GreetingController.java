b03cf014d4c306fff71735b51f433f95ad1aa27e
danielmenezesbr <danielmenezes@gmail.com>
2025-04-23 19:46:19 -0300

    Update WebSocket configuration and enhance greeting response

--- complete/src/main/java/com/example/messagingstompwebsocket/GreetingController.java
+++ complete/src/main/java/com/example/messagingstompwebsocket/GreetingController.java
@@ -1,19 +1,38 @@
 package com.example.messagingstompwebsocket;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.config.WebSocketMessageBrokerStats;
 import org.springframework.web.util.HtmlUtils;
+
+import javax.annotation.PostConstruct;
 
 @Controller
 public class GreetingController {
+
+	@Autowired
+	private WebSocketMessageBrokerStats brokerStats;
+
+	@Value("${spring.application.version}")
+	private String appVersion;
+
+	@PostConstruct
+	public void init() {
+		System.out.println("brokerStats:");
+		System.out.println(brokerStats);
+	}
 
 
 	@MessageMapping("/hello")
 	@SendTo("/topic/greetings")
 	public Greeting greeting(HelloMessage message) throws Exception {
 		Thread.sleep(1000); // simulated delay
-		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+		System.out.println("Received message: " + message.getName());
+		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) +
+				"! (hostname: %s, version: %s)".formatted(System.getenv("HOSTNAME"), appVersion));
 	}
 
 }
--- complete/src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
+++ complete/src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package com.example.messagingstompwebsocket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -10,15 +12,26 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+	@Bean
+	public ThreadPoolTaskScheduler stompTaskScheduler() {
+		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+		scheduler.setPoolSize(100);
+		scheduler.setThreadNamePrefix("stomp-scheduler-");
+		scheduler.setWaitForTasksToCompleteOnShutdown(true);
+		scheduler.setAwaitTerminationSeconds(30);
+		return scheduler;
+	}
+
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic");
+		config.enableSimpleBroker("/topic")
+				.setTaskScheduler(stompTaskScheduler())
+				.setHeartbeatValue(new long[]{10000, 10000});
 		config.setApplicationDestinationPrefixes("/app");
 	}
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/gs-guide-websocket");
+		registry.addEndpoint("/ws");
 	}
-
 }
